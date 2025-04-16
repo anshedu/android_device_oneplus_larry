5c2f417120c40e52147c75bdb0bd8d136bfd977a
Vivekachooz <vivekachooz@gmail.com>
2025-04-16 12:50:47 +0530

    larry: Update from CPH2467_14.0.0.1800(EX01)
    Change-Id: Icba4d0d8d35fb6fb6c25927462a1730fa25a6ecd

--- lineage_larry.mk
+++ lineage_larry.mk
@@ -25,8 +25,8 @@ PRODUCT_MANUFACTURER := oneplus
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="OP5958L1-user 14 UKQ1.230924.001 T.R4T2.1b588e3_1-ba57a release-keys" \
-    BuildFingerprint=OnePlus/CPH2467/OP5958L1:14/UKQ1.230924.001/T.R4T2.1b588e3_1-ba57a:user/release-keys \
+    BuildDesc="OP5958L1-user 14 UKQ1.230924.001 T.R4T2.1cd4793-227bc-40edf release-keys" \
+    BuildFingerprint=OnePlus/CPH2467/OP5958L1:14/UKQ1.230924.001/T.R4T2.1cd4793-227bc-40edf:user/release-keys \
     DeviceName=OP5958L1 \
     DeviceProduct=OP5958L1 \
     SystemDevice=OP5958L1 \
